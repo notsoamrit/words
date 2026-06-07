ea30081259f539caa6e8f7e176152bce4c8a5c10
notsoamrit <amrit.pdf@gmail.com>
2026-06-07 23:34:47 +0530

    Update supabase.sql

--- supabase.sql
+++ supabase.sql
@@ -1,6 +1,9 @@
-create table searched_words (
+create table words (
   id bigint generated always as identity primary key,
-  word text not null,
-  explanation text,
-  searched_at timestamptz default now()
+  word text unique not null,
+  explanation text not null,
+  is_favorite boolean default false,
+  created_at timestamptz default now()
 );
+
+create index words_word_idx on words(word);
